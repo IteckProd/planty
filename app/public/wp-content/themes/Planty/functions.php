f4c0de128f53cbac51ebd98c3ae44e77813f1a94
Louis <louis-lyonnais@live.fr>
2023-06-14 15:24:40 +0200

    changes for validation

--- app/public/wp-content/themes/Planty/functions.php
+++ app/public/wp-content/themes/Planty/functions.php
@@ -25,22 +25,19 @@ function afficher_lien_admin_menu() {
     if (is_user_logged_in()) {
         // L'utilisateur est connecté, affichez le lien vers l'admin
         ?>
-        <li><a href="<?php echo admin_url(); ?>">Admin</a></li>
+        <li class="admin_button" ><a href="<?php echo admin_url(); ?>">Admin</a></li>
         <?php
     }
 }
-
 function ajouter_lien_admin_menu($items, $args) {
     ob_start();
     afficher_lien_admin_menu();
     $lien_admin_menu = ob_get_clean();
 
-    // Ajouter le lien vers l'admin à la fin du menu
-    $items .= $lien_admin_menu;
+    // Remplacer le lien vers l'admin à la fin du menu par le début du menu
+    $items = str_replace($lien_admin_menu, '', $items);
+    $items = $lien_admin_menu . $items;
     return $items;
 }
 add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2);
-
-
-
 // END ENQUEUE PARENT ACTION
